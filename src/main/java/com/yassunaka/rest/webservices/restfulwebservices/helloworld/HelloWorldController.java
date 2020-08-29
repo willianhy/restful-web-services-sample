17720dffce9f2490fbc297b39bbab2047ffdd233
Willian Yassunaka <willianhy@gmail.com>
2020-08-29 20:18:38 -0300

    internationalization

--- src/main/java/com/yassunaka/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/yassunaka/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,13 +1,19 @@
 package com.yassunaka.rest.webservices.restfulwebservices.helloworld;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 import static java.lang.String.format;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @RequestMapping(path = "/hello-world")
     public String helloWorld(){
@@ -22,5 +28,10 @@ public class HelloWorldController {
     @RequestMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean(format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
